3ab01a6d4c9fe391f1c0da539c5a3fea55370ee6
Evan <43421690+WackyFolf@users.noreply.github.com>
2020-10-09 23:42:21 -0600

    Text entery box

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 # Copyright (c) 2020 Evan Sonin
 import tkinter
-from tkinter import Button, Message
+from tkinter import Button, Message, Label, Entry
 
 window = tkinter.Tk() # Create new Tkinter window
 window.title("Testing Program") # Set the title of the window
-def printInConsole(): # Create new command for the button to execute
-    print ("Hello there")
-hiButton = tkinter.Button (window, text="Hi", command = printInConsole) # Create button with the text "text" in the window "window" that executes printInConsole.
-label = Message(window, text="The server is running but no one is allowed on, so if you see that it has an unusually low amount of people, it's whitelisted still. ") # Chunk of text.
-hiButton.pack() # Add button to window
+def setLabel(): # Create new command for the button to execute
+    newText = entry.get() # Get text from text box
+    label["text"] = newText
+hiButton = Button(window, text="Hi", command = setLabel) # Create button with the text "text" in the window "window" that executes printInConsole.
+label = Label(window, text="The server is running but no one is allowed on, \n so if you see that it has an unusually low amount of people, \nit's whitelisted still.") # Chunk of text.
+entry = Entry()
 label.pack() # Add text to window
+hiButton.pack() # Add button to window
+entry.pack()
 window.mainloop() # Open window
