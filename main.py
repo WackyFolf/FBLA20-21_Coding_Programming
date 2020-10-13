b8e2e75842a4dfa7853f2d88f48403601b2e2dc1
Evan Sonin <43421690+WackyFolf@users.noreply.github.com>
2020-10-12 22:43:35 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 # Copyright (c) 2020 Evan Sonin
 import tkinter
-from tkinter import Button, Message, Label, Entry
+from tkinter import *
 
-window = tkinter.Tk() # Create new Tkinter window
-window.title("Testing Program") # Set the title of the window
+root = tkinter.Tk() # Create new Tkinter window
+root.title("Testing Program") # Set the title of the window
 def setLabel(): # Create new command for the button to execute
     newText = entry.get() # Get text from text box
     label["text"] = newText
-hiButton = Button(window, text="Hi", command = setLabel) # Create button with the text "text" in the window "window" that executes printInConsole.
-label = Label(window, text="The server is running but no one is allowed on, \n so if you see that it has an unusually low amount of people, \nit's whitelisted still.") # Chunk of text.
-entry = Entry()
+frame = Frame(relief = SUNKEN, master=root)
+frame2 = Frame(master=root, height=50)
+hiButton = Button(root, text="Hi", command = setLabel) # Create button with the text "text" in the window "window" that executes printInConsole.
+label = Label(text="The server is running but no one is allowed on, \n so if you see that it has an unusually low amount of people, \nit's whitelisted still.", master=frame, relief=RIDGE) # Chunk of text.
+entry = Entry(master = frame2)
+frame.pack(side=tkinter.TOP)
+frame2.pack(side=tkinter.LEFT)
 label.pack() # Add text to window
-hiButton.pack() # Add button to window
+hiButton.pack(side=tkinter.LEFT) # Add button to window
 entry.pack()
-window.mainloop() # Open window
+root.mainloop() # Open window
