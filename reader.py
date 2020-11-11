811902eb15e855e7e9a24fdc9285a2117aec38e6
Evan Sonin <43421690+WackyFolf@users.noreply.github.com>
2020-11-11 16:37:42 -0700

    Update reader.py

--- reader.py
+++ reader.py
@@ -1,11 +1,12 @@
 import json
 
 class reader:
-    def __init__(self, want, num):
+    def __init__(self, want, num, quiz):
         self.want = want
         self.num = num
-    def read(self, want, num):
-        with open ("quizzes/test.json", "r") as f:  # Open file for reading
+        self.quiz = quiz
+    def read(self, want, num, quiz):
+        with open ("quizzes/"+quiz+".json", "r") as f:  # Open file for reading
             data = json.load(f)
         return(data[num][want]) # Return data from JSON to UI program
             
