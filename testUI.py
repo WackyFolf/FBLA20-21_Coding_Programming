239adea98945162bd5dd2c7b54649334084263d4
Evan Sonin <43421690+WackyFolf@users.noreply.github.com>
2020-11-11 09:08:25 -0700

    Update testUI.py

--- testUI.py
+++ testUI.py
@@ -1,7 +1,10 @@
 import tkinter
 from tkinter import *
 import sys
+from reader import reader
+from functools import partial
 
+reader = reader("ques", "1")
 root = Tk()
 root.title('UI Testing')
 frame = LabelFrame(root, padx=5, pady=5, text="Start a Quiz")
@@ -11,11 +14,17 @@ frame2.pack(padx=10, pady=10, side = RIGHT)
 
 def exitProgram():
     sys.exit() 
-b = Button(frame, text="Button")
+def obtainInfo(want, num):
+    label.config(text = (reader.read(want, num)))
+    print(reader.read("ques", "2"))
+b = Button(frame, text="Question 1", command=partial(obtainInfo, "ques", "1"))
 b.grid(row=1, column=0)
-b2 = Button(frame, text="Button")
+b2 = Button(frame, text="Question 2", command=partial(obtainInfo, "ques", "2"))
 b2.grid(row=1, column=1)
+b3 = Button(frame, text="Question 3", command=partial(obtainInfo, "ques", "3"))
+b3.grid(row=1, column=2)
 b3 = Button(frame2, text="Exit", command=exitProgram)
 b3.grid(row=0, column=0)
-label = Label(frame, text="I am a label").grid(row=0,column=0)
+label = Label(frame, text="Click a button for a question.")
+label.grid(row=0,column=0)
 root.mainloop()
