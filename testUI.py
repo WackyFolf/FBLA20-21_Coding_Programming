a703c26f9383e80e49241d1f431c4824f7a254a7
Evan Sonin <43421690+WackyFolf@users.noreply.github.com>
2020-11-11 17:08:31 -0700

    Update testUI.py

--- testUI.py
+++ testUI.py
@@ -15,12 +15,20 @@ variable.set("Question") # Sets default value
 
 frame = LabelFrame(root, padx=5, pady=5, text="Start a Quiz")
 frame.pack(padx=10, pady=10, side = LEFT)
-frame2 = LabelFrame(root, padx=5, pady=5, text="End This Program")
+frame2 = LabelFrame(root, padx=5, pady=5, text="Other")
 frame2.pack(padx=10, pady=10, side = RIGHT)
 
 def exitProgram():
-    quizUI.openQuiz("quiz")
     sys.exit() 
+def license():
+    with open ("LICENSE", "r") as licenseFile:
+        licenseText = licenseFile.read()
+    licenseWindow = Tk()
+    licenseWindow.resizable(width=False, height=False)
+    licenseWindow.iconbitmap('icons/license.ico')
+    licenseWindow.title('© 2020 Evan Sonin')
+    labelLicense = Label(licenseWindow, text=licenseText).pack()
+    licenseWindow.mainloop()
 def obtainInfo(num):
     want = optionDict.get(variable.get()) # Define want as the dictionary definition for the current variable of the dropdown
     label.config(text = (reader.read(want, num, "computerapps")))
@@ -40,7 +48,9 @@ b2.grid(row=1, column=1)
 b3 = Button(frame, text="Question 3", command=partial(obtainInfo, "3"))
 b3.grid(row=1, column=2)
 b3 = Button(frame2, text="Exit", command=exitProgram)
-b3.grid(row=0, column=0)
+b3.grid(row=0, column=1, padx=10, pady=10)
+b3 = Button(frame2, text="License", command=license)
+b3.grid(row=0, column=0, padx=10, pady=10)
 label = Label(frame, text="Click a button for a question.")
 label.grid(row=0,column=0)
 root.mainloop()
